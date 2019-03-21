dfcaee82db239d4a40ce1e4e613b74bef42a3a20
Aleksi Alajoki <fensnake@yahoo.com>
2019-03-21 22:48:57 +0200

    Cleaned up testing data insertion statements

--- sql/insert_testing_data.sql
+++ sql/insert_testing_data.sql
@@ -2,57 +2,48 @@ use roleplay_db;
 
 -- Default system data
 insert into Character_Class (name)  
-values ("Warrior");
-insert into Character_Class (name)  
-values ("Rogue");
-insert into Character_Class (name)  
-values ("Mage");
+values 
+("Warrior"), 
+("Rogue"), 
+("Mage");
 
 insert into Character_Race (name)  
-values ("Human");
-insert into Character_Race (name)  
-values ("Dwarf");
-insert into Character_Race (name)  
-values ("Elf");
+values 
+("Human"), 
+("Dwarf"), 
+("Elf");
 
 insert into Guild_Type (name)  
-values ("PvE");
-insert into Guild_Type (name)  
-values ("PvP");
-insert into Guild_Type (name)  
-values ("RP");
+values 
+("PvE"), 
+("PvP"), 
+("RP");
 
 insert into Guild_Rank (name)  
-values ("Initiate");
-insert into Guild_Rank (name)  
-values ("Member");
-insert into Guild_Rank (name)  
-values ("Senior");
-insert into Guild_Rank (name)  
-values ("Officer");
-insert into Guild_Rank (name)  
-values ("Guild Master");
+values 
+("Initiate"), 
+("Member"), 
+("Senior"), 
+("Officer"), 
+("Guild Master");
+
 
 
 -- Player data
 insert into Player_Character (name, class, race, level)  
-values ("Rob Johnson", 1, 1, 21);
-insert into Player_Character (name, class, race, level)  
-values ("Long-Ears", 3, 3, 5);
-insert into Player_Character (name, class, race)  
-values ("Ironpants", 1, 2);
-insert into Player_Character (name, class, race, level)  
-values ("Sneaky", 2, 3, 24);
+values 
+("Rob Johnson", 1, 1, 21), 
+("Long-Ears", 3, 3, 5), 
+("Ironpants", 1, 2, 1),
+("Sneaky", 2, 3, 24);
 
 insert into Guild (name, type, description)  
-values ("Explorer's Guild", 1, "A casual guild for fun-loving people.");
-insert into Guild (name, type, description)  
-values ("Sneaky Bandits", 3, "We steal stuff.");
+values 
+("Explorer's Guild", 1, "A casual guild for fun-loving people."),
+("Sneaky Bandits", 3, "We steal stuff.");
 
-
 insert into Guild_Membership (guild_id, character_id, rank)  
-values (1, 3, 5);
-insert into Guild_Membership (guild_id, character_id, rank)  
-values (1, 1, 4);
-insert into Guild_Membership (guild_id, character_id, rank)  
-values (2, 4, 5);
+values 
+(1, 3, 5),
+(1, 1, 4),
+(2, 4, 5);
